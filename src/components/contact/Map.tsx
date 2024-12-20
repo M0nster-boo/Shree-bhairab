cf853363ffa6bae68abd0730e69170bcddf7e5bf
M0nster-boo <mhz.pradeep@gmail.com>
2024-12-20 16:37:13 +0545

    Update Map.tsx

--- src/components/contact/Map.tsx
+++ src/components/contact/Map.tsx
@@ -3,7 +3,6 @@ export default function Map() {
     <div className="h-[400px] rounded-lg overflow-hidden">
       <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d220.78722394985965!2d85.3044191!3d27.6988917!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb19f953679daf%3A0xa546d7de40ff003c!2sTanani!5e0!3m2!1sen!2snp!4v1734691049427!5m2!1sen!2snp" 
         width="600" height="450" 
-        style="border:0;" 
         allowfullscreen="" 
         loading="lazy" 
         referrerpolicy="no-referrer-when-downgrade">

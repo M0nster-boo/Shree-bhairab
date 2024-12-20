e78abfcde477c3cc939b773108a1567be67d00b2
M0nster-boo <mhz.pradeep@gmail.com>
2024-12-20 16:23:15 +0545

    Update Map.tsx

--- src/components/contact/Map.tsx
+++ src/components/contact/Map.tsx
@@ -1,14 +1,13 @@
 export default function Map() {
   return (
     <div className="h-[400px] rounded-lg overflow-hidden">
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2624.9916256937595!2d2.2922926!3d48.8583736!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e2964e34e2d%3A0x8ddca9ee380ef7e0!2sEiffel%20Tower!5e0!3m2!1sen!2sus!4v1647533374815!5m2!1sen!2sus"
-        width="100%"
-        height="100%"
-        style={{ border: 0 }}
-        allowFullScreen
-        loading="lazy"
-      ></iframe>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d220.78722394985965!2d85.3044191!3d27.6988917!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb19f953679daf%3A0xa546d7de40ff003c!2sTanani!5e0!3m2!1sen!2snp!4v1734691049427!5m2!1sen!2snp" 
+        width="600" height="450" 
+        style="border:0;" 
+        allowfullscreen="" 
+        loading="lazy" 
+        referrerpolicy="no-referrer-when-downgrade">
+      </iframe>
     </div>
   );
 }

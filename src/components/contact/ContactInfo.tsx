d62ad244ab5b60f72c475be8e06bcde785fe4886
M0nster-boo <mhz.pradeep@gmail.com>
2024-12-20 16:22:07 +0545

    Update ContactInfo.tsx

--- src/components/contact/ContactInfo.tsx
+++ src/components/contact/ContactInfo.tsx
@@ -9,21 +9,21 @@ export default function ContactInfo() {
           <MapPin className="text-amber-500" size={24} />
           <div>
             <p className="font-semibold">Location</p>
-            <p className="text-gray-600">123 Music Avenue, Harmony City</p>
+            <p className="text-gray-600">Ta:nani-12, Kathmandu, Nepal</p>
           </div>
         </div>
         <div className="flex items-center space-x-4">
           <Phone className="text-amber-500" size={24} />
           <div>
             <p className="font-semibold">Phone</p>
-            <p className="text-gray-600">+1 (555) 123-4567</p>
+            <p className="text-gray-600">+977 9863689662</p>
           </div>
         </div>
         <div className="flex items-center space-x-4">
           <Mail className="text-amber-500" size={24} />
           <div>
             <p className="font-semibold">Email</p>
-            <p className="text-gray-600">contact@harmonicfusion.com</p>
+            <p className="text-gray-600">shreebhairabsmai@gmail.com</p>
           </div>
         </div>
       </div>
